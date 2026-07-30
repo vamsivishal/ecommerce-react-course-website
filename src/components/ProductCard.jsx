aae24113144a962bfd28f2d06b3bd911042cf44d
vishal <vamsivishal97@gmail.com>
2026-07-30 17:54:44 +0530

    Fixed image

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,7 +4,7 @@ export default function ProductCard({ product }) {
     return ( 
     <>
                     <div className="product-card" >
-                        <img src={product.image} className="product-card-image" />
+                        <img src={product.image} alt={product.name} className="product-card-image" />
                     <div className="product-card-content">
                             <h3 className="product-card-name">{product.name}</h3>
                             <p className="product-card-price">${product.price.toFixed(2)}</p>
