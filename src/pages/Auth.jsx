34eb494ed6f7a892bc44a19fa2b4f87f6273bea7
vishal <vamsivishal97@gmail.com>
2026-09-07 21:06:00 +0530

    Added Home Page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react"
+
+
 export default function Auth() {
+   const [mode, setMode] = useState("signup")
+
   return (
-    <div>Auth Page</div>
+    <div className="page">
+      <div className="container">
+        <div className="auth-container">
+          <h1 className="page-title">{mode === "signup" ? "Sign up" : "Login"}</h1>
+          <form className="auth-form">
+            <div className="form-group">
+              <label className="form-label" htmlFor="email">Email</label>
+              <input className="form-input" type="email" id="email" />
+            </div>
+            <div className="form-group">
+              <label className="form-label" htmlFor="password">Password</label>
+              <input className="form-input" type="password" id="password" />
+            </div>
+
+            <button className="btn btn-primary btn-large" type="submit">{mode === "signup" ? "Sign up" : "Login"}</button>
+          </form>
+
+          <div className="auth-switch">
+            {mode === "signup" ? (
+              <p>Already have an account? <span className="auth-link" onClick={() => setMode("login")}>Login</span></p>
+            ) : (
+              <p>Don't have an account? <span className="auth-link" onClick={() => setMode("signup")}>Sign up</span></p>
+            )}
+          </div>
+       
+
+        </div>
+      </div>
+    </div>
   )
 }
